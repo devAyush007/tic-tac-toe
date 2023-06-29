30ad64af3f4306e1c98bb9b73d5a8ddd22ab1eaa
Ayushoopp <ayushmehrotra099@>
2023-06-29 20:30:45 +0530

    Completed Aprox

---
+++ components/Square.tsx
@@ -0,0 +1,20 @@
+function Square({ value, onClick, winner }: {
+  winner: string | null;
+  value: string | null;
+  onClick: () => void;
+}) {
+  if (!value) {
+    return (
+      <button className="square" onClick={onClick} disabled={Boolean(winner)}>
+        |
+        
+      </button>
+    );
+  }
+
+  return (
+    <button className={`square square_${value.toLowerCase()}`} disabled>{value}</button>
+  );
+}
+
+export default Square;
